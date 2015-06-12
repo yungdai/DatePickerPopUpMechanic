5f254f16232797375b231d8ac6d0f9815300d59c
Yung Dai <yungchidai@gmail.com>
2015-06-12 12:08:50 -0400

    Initial Commit

---
+++ DatePickerPopUpMechanic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DatePickerPopUpMechanic
+//
+//  Created by Yung Dai on 2015-06-12.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
